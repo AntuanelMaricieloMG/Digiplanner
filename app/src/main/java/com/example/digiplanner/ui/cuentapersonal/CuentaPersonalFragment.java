390b6109ace86d96271b79ee3257938ddeacd531
Maricielo <cac.02.antuanel.medina.garagatte@gmail.com>
2022-11-15 12:06:33 +0100

    Menú Cuental Personal , datos

--- app/src/main/java/com/example/digiplanner/ui/cuentapersonal/CuentaPersonalFragment.java
+++ app/src/main/java/com/example/digiplanner/ui/cuentapersonal/CuentaPersonalFragment.java
@@ -1,27 +1,43 @@
 package com.example.digiplanner.ui.cuentapersonal;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 
-import com.example.digiplanner.databinding.FragmentCuentapersonalBinding;
+
+import com.example.digiplanner.R;
+
 
 
 public class CuentaPersonalFragment extends Fragment {
 
-    private FragmentCuentapersonalBinding fragmentC;
+    ImageView imagenCuenta;
+    TextView textoNombre;
+    TextView textoCorreo;
+    TextView textoContraseña;
+    TextView textoNotificaciones;
+    TextView textoCerrar;
+    TextView textoBorrar;
+    //ºprivate FragmentCuentapersonalBinding fragmentC;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @SuppressLint("MissingInflatedId")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        CuentaPersonalViewModel cuentaPersonalViewModel =
+        /*CuentaPersonalViewModel cuentaPersonalViewModel =
                 new ViewModelProvider(this).get(CuentaPersonalViewModel.class);
 
         fragmentC = FragmentCuentapersonalBinding.inflate(inflater, container, false);
@@ -29,12 +45,25 @@ public class CuentaPersonalFragment extends Fragment {
         //elementos del layout
         final TextView textView = fragmentC.textCuenta;
         cuentaPersonalViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return root;*/
+        View view = inflater.inflate(R.layout.fragment_cuentapersonal,container,false);
+
+        //Elementos por ID
+        imagenCuenta = (ImageView) view.findViewById(R.id.imageCuenta);
+        textoNombre = (TextView) view.findViewById(R.id.textNombre);
+        textoCorreo = (TextView) view.findViewById(R.id.textCorreo);
+        textoContraseña = (TextView) view.findViewById(R.id.textContraseña);
+        textoNotificaciones = (TextView) view.findViewById(R.id.textNotificaciones);
+        textoCerrar = (TextView) view.findViewById(R.id.textCerrar);
+        textoBorrar = (TextView) view.findViewById(R.id.textBorrar);
+        return view;
     }
 
-    @Override
+
+
+    /*@Override
     public void onDestroyView() {
         super.onDestroyView();
         fragmentC = null;
-    }
+    }*/
 }
