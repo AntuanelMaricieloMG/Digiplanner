49db53f52141b310b5c5fbde4ef640ca65cae394
Maricielo <cac.02.antuanel.medina.garagatte@gmail.com>
2022-11-18 13:27:24 +0100

    Usuario nuevo puede registrarse y acceder a la app

--- app/src/main/java/com/example/digiplanner/AccederAppNuevoUsuario.java
+++ app/src/main/java/com/example/digiplanner/AccederAppNuevoUsuario.java
@@ -1,5 +1,6 @@
 package com.example.digiplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -7,13 +8,21 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AccederAppNuevoUsuario extends AppCompatActivity {
 
-    EditText nuevoEmail,nuevaContraseña;
-    RelativeLayout nuevoUsuario;
-    TextView accede;
+    private EditText nuevoEmail,nuevaContraseña;
+    private RelativeLayout nuevoUsuario;
+    private TextView accede;
+    private FirebaseAuth firebaseAutentificacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +30,13 @@ public class AccederAppNuevoUsuario extends AppCompatActivity {
         setContentView(R.layout.principal_acceder_app_nuevousuario);
         getSupportActionBar().hide();
 
+        //id de elementos Layouts
         nuevoEmail = findViewById(R.id.text_login_nuevo_email);
         nuevaContraseña = findViewById(R.id.text_login_nueva_contraseña);
         nuevoUsuario = findViewById(R.id.login_nuevo_usuario);
         accede = findViewById(R.id.tengo_un_usuario);
+
+        firebaseAutentificacion = FirebaseAuth.getInstance();
 
         accede.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,9 +54,44 @@ public class AccederAppNuevoUsuario extends AppCompatActivity {
                 }
                 else
                 {
-
+                    //se registra usuario nuevo
+                    firebaseAutentificacion.createUserWithEmailAndPassword(emailnuevo,contrasseñanueva).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(getApplicationContext(),"Registro completo",Toast.LENGTH_SHORT).show();
+                                sendEmail();
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"Fallo", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
             }
         });
+    }
+
+    private void sendEmail(){
+
+        FirebaseUser firebaseUsuario = firebaseAutentificacion.getCurrentUser();
+        if(firebaseUsuario != null)
+        {
+            firebaseUsuario.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(getApplicationContext(), "verificacion hecha, entra otra vez", Toast.LENGTH_SHORT).show();
+                    firebaseAutentificacion.signOut();
+                    finish();
+                    startActivity(new Intent(AccederAppNuevoUsuario.this,AccederApp.class));
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Fallo", Toast.LENGTH_SHORT).show();
+        }
     }
 }
